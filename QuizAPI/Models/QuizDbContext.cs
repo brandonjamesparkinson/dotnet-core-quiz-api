8d7c724e84d60086fd98456349ca068076947c6a
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2022-03-18 14:56:33 +0000

    + scaffolded api controllers

--- QuizAPI/Models/QuizDbContext.cs
+++ QuizAPI/Models/QuizDbContext.cs
@@ -5,9 +5,7 @@ namespace QuizAPI.Models
     public class QuizDbContext : DbContext
     {
         public QuizDbContext(DbContextOptions<QuizDbContext> options) : base(options)
-        {
-
-        }
+        {  }
 
         public DbSet<Question> Questions { get; set; }
         public DbSet<Participant> Participants { get; set; }
