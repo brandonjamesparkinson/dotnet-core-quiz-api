5767ba14e8fbff63e9ac177643914a460eaf5123
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2022-03-21 17:57:07 +0000

    get answers rework (renamed and refactored)

--- QuizAPI/Controllers/QuestionController.cs
+++ QuizAPI/Controllers/QuestionController.cs
@@ -31,7 +31,7 @@ namespace QuizAPI.Controllers
                     QnId = x.QnId,
                     QnInWords = x.QnInWords,
                     ImageName = x.ImageName,
-                    options = new string[] { x.Option1, x.Option2, x.Option3, x.Option4 }
+                    Options = new string[] { x.Option1, x.Option2, x.Option3, x.Option4 }
                 })
                 .OrderBy(y => Guid.NewGuid())
                 .Take(5).ToListAsync());
@@ -84,15 +84,23 @@ namespace QuizAPI.Controllers
             return NoContent();
         }
 
-        // POST: api/Question
+        // POST: api/Question/GetAnswers
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Question>> PostQuestion(Question question)
+        [Route("GetAnswers")]
+        public async Task<ActionResult<Question>> RetrieveAnswers(int[] qnIds)
         {
-            _context.Questions.Add(question);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetQuestion", new { id = question.QnId }, question);
+            var answers = await (_context.Questions
+                .Where(x => qnIds.Contains(x.QnId))
+                .Select(y => new
+                {
+                    QnId = y.QnId,
+                    QnInWords = y.QnInWords,
+                    ImageName = y.ImageName,
+                    Options = new string[] { y.Option1, y.Option2, y.Option3, y.Option4 },
+                    Answer = y.Answer
+                })).ToListAsync();
+            return Ok(answers);
         }
 
         // DELETE: api/Question/5
